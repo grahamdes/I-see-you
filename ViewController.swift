7e9ae6930f77543b5216edf9279d81ffe826b6eb
grahamdes <Graham@Grahams-Air.home>
2016-06-21 21:47:13 -0700

    yo
    hello

---
+++ ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  I see you
+//
+//  Created by Graham Desmond on 6/16/16.
+//  Copyright © 2016 GrahamApps. All rights reserved.
+//
+
+import UIKit
+
+ var toDoList = [String]()
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet var toDoListTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+            
+            toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+            
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return toDoList.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            toDoList.removeAtIndex(indexPath.row)
+            
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            
+            toDoListTable.reloadData()
+        }
+        
+        
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        toDoListTable.reloadData()
+        
+    }
+    
+    
+    
+}
+
+
+
+
+}
+
